adb59d37ed58eb178d35a0771375910862276851
Tomass Yoga <tomass.petersons96@gmail.com>
2022-04-27 17:12:38 +0300

    Add "hover" effect on grid

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const sketchContainer = document.querySelector('.sketch');
 
 let squareCount = 16;
 
-for (i=1; i <= squareCount; i++) {
+for (i = 1; i <= squareCount; i++) {
   // Create Sketch row
   const sketchRow = document.createElement('div');
   sketchRow.classList.add('sketch__row', `sketch__row--${i}`);
   sketchContainer.appendChild(sketchRow);
-  for (j=1; j <= squareCount; j++) {
+  for (j = 1; j <= squareCount; j++) {
     // Create Square
     const square = document.createElement('div');
-    square.classList.add('square', `square-${j}`);
+    square.classList.add('square', `square--${i}__${j}`);
     sketchRow.appendChild(square);
   }
 }
+
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    square.classList.add('square--hover');
+  });
+});
